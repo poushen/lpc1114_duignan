2d4cf6771cb1b3e8cef4c3a8585e12fd99915214
Phou-shen Ou <31812245+poushen@users.noreply.github.com>
2018-06-08 07:00:02 +0800

    mark out the test global variable line

--- main.c
+++ main.c
@@ -3,18 +3,18 @@
 // Written by Ted Burke (based on code by Frank Duignan)
 // Last updated 29-11-2013
 //
- 
+
 #include "lpc111x.h"
 #include <stdint.h>
- 
-uint8_t global[30];
+
+//uint8_t global[30];
 int main()
 {
     // Turn on clock for GPIO, IOCON and ADC
     SYSAHBCLKCTRL |= BIT6 + BIT13 + BIT16;
     GPIO0DIR = BIT8; // Make PIO0_8 an output
     GPIO0DATA = 0;   // Turn off PIO0 outputs
-     
+
     int n;
     while(1)
     {
@@ -22,5 +22,5 @@ int main()
         n=1000000; while(--n);
         GPIO0DATA = 0;    // Turn on PIO0_8
         n=1000000; while(--n);
-    }    
+    }
 }
